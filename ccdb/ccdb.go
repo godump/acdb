23db9f298f1ad8a6a3e0f3b70f8b3d1989f5d064
mohanson <mohanson@outlook.com>
2018-09-14 16:38:52 +0800

    2018-09-14 16:38:52

--- ccdb/ccdb.go
+++ ccdb/ccdb.go
@@ -1,15 +1,5 @@
 package ccdb
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"net/http"
-	"strconv"
-
-	"github.com/mohanson/acdb"
-)
-
 type Option struct {
 	Command string `json:"command"`
 	K       string `json:"k"`
@@ -22,83 +12,83 @@ type Output struct {
 	V   []byte `json:"v"`
 }
 
-func NewEmerge(server string) *Emerge {
-	return &Emerge{
-		server: server,
-		client: http.DefaultClient,
-	}
-}
+// func NewEmerge(server string) *Emerge {
+// 	return &Emerge{
+// 		server: server,
+// 		client: http.DefaultClient,
+// 	}
+// }
 
-type Emerge struct {
-	server string
-	client *http.Client
-}
+// type Emerge struct {
+// 	server string
+// 	client *http.Client
+// }
 
-func (e *Emerge) Cmd(option *Option) *Output {
-	output := &Output{}
-	buf, err := json.Marshal(option)
-	if err != nil {
-		output.Err = err.Error()
-		return output
-	}
-	req, err := http.NewRequest("PUT", e.server, bytes.NewReader(buf))
-	if err != nil {
-		output.Err = err.Error()
-		return output
-	}
-	res, err := e.client.Do(req)
-	if err != nil {
-		output.Err = err.Error()
-		return output
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		output.Err = errors.New(strconv.Itoa(res.StatusCode))
-		return output
-	}
-	if err := json.NewDecoder(res.Body).Decode(output); err != nil {
-		output.Err = err.Error()
-		return output
-	}
-	return output
-}
+// func (e *Emerge) Cmd(option *Option) *Output {
+// 	output := &Output{}
+// 	buf, err := json.Marshal(option)
+// 	if err != nil {
+// 		output.Err = err.Error()
+// 		return output
+// 	}
+// 	req, err := http.NewRequest("PUT", e.server, bytes.NewReader(buf))
+// 	if err != nil {
+// 		output.Err = err.Error()
+// 		return output
+// 	}
+// 	res, err := e.client.Do(req)
+// 	if err != nil {
+// 		output.Err = err.Error()
+// 		return output
+// 	}
+// 	defer res.Body.Close()
+// 	if res.StatusCode != 200 {
+// 		output.Err = errors.New(strconv.Itoa(res.StatusCode))
+// 		return output
+// 	}
+// 	if err := json.NewDecoder(res.Body).Decode(output); err != nil {
+// 		output.Err = err.Error()
+// 		return output
+// 	}
+// 	return output
+// }
 
-func (e *Emerge) Get(k string, v interface{}) error {
-	output := e.Cmd(&Option{Command: "GET", K: k})
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(output.V, v)
-}
+// func (e *Emerge) Get(k string, v interface{}) error {
+// 	output := e.Cmd(&Option{Command: "GET", K: k})
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return json.Unmarshal(output.V, v)
+// }
 
-func (e *Emerge) Set(k string, v interface{}) error {
-	buf, err := json.Marshal(v)
-	if err != nil {
-		return err
-	}
-	_, err = e.Cmd(&Option{Command: "SET", K: k, V: buf})
-	return err
-}
+// func (e *Emerge) Set(k string, v interface{}) error {
+// 	buf, err := json.Marshal(v)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	_, err = e.Cmd(&Option{Command: "SET", K: k, V: buf})
+// 	return err
+// }
 
-func (e *Emerge) Del(k string) {
-	option := &Option{Command: "DEL", K: k}
-	e.Cmd(option)
-}
+// func (e *Emerge) Del(k string) {
+// 	option := &Option{Command: "DEL", K: k}
+// 	e.Cmd(option)
+// }
 
-func (e *Emerge) Add(k string, n int64) error {
-	buf, _ := json.Marshal(n)
-	option := &Option{Command: "ADD", K: k, V: buf}
-	_, err := e.Cmd(option)
-	return err
-}
+// func (e *Emerge) Add(k string, n int64) error {
+// 	buf, _ := json.Marshal(n)
+// 	option := &Option{Command: "ADD", K: k, V: buf}
+// 	_, err := e.Cmd(option)
+// 	return err
+// }
 
-func (e *Emerge) Dec(k string, n int64) error {
-	buf, _ := json.Marshal(n)
-	option := &Option{Command: "DEC", K: k, V: buf}
-	_, err := e.Cmd(option)
-	return err
-}
+// func (e *Emerge) Dec(k string, n int64) error {
+// 	buf, _ := json.Marshal(n)
+// 	option := &Option{Command: "DEC", K: k, V: buf}
+// 	_, err := e.Cmd(option)
+// 	return err
+// }
 
-func Cli(server string) acdb.Client {
-	return NewEmerge(server)
-}
+// func Cli(server string) acdb.Client {
+// 	return NewEmerge(server)
+// }
--- ccdb/cmd/ccdb/main.go
+++ ccdb/cmd/ccdb/main.go
@@ -14,17 +14,63 @@ import (
 )
 
 var (
-	flDriver = flag.String("driver", "lru", "Driver")
-	flDoc    = flag.String("doc", path.Join(os.TempDir(), "acdb"), "Doc root")
-	flCap    = flag.Int("cap", 1024, "LRU cache size")
-	flListen = flag.String("l", ":8080", "Listen address")
+	flDriverMem = flag.Bool("driver-mem", false, "Use acdb.Mem() for driver")
+	flDriverDoc = flag.Bool("driver-doc", false, "Use acdb.Doc() for driver")
+	flDriverLRU = flag.Bool("driver-lru", false, "Use acdb.LRU() for driver")
+	flDriverMap = flag.Bool("driver-map", false, "Use acdb.Map() for driver")
+	flPath      = flag.String("path", path.Join(os.TempDir(), "acdb"), "Directory to store data")
+	flSize      = flag.Int("size", 1024, "Database size")
+	flListen    = flag.String("l", ":8080", "Listen address")
 )
 
 var (
 	client acdb.Client
 )
 
-func hand(w http.ResponseWriter, r *http.Request) {
+func serveGet(option *ccdb.Option, output *ccdb.Output) {
+	var raw json.RawMessage
+	if err := client.Get(option.K, &raw); err != nil {
+		output.Err = err.Error()
+		return
+	}
+	output.V = raw
+}
+
+func serveSet(option *ccdb.Option, output *ccdb.Output) {
+	if err := client.Set(option.K, json.RawMessage(option.V)); err != nil {
+		output.Err = err.Error()
+	}
+}
+
+func serveDel(option *ccdb.Option, output *ccdb.Output) {
+	client.Del(option.K)
+}
+
+func serveAdd(option *ccdb.Option, output *ccdb.Output) {
+	var n int64
+	if err := json.Unmarshal(option.V, &n); err != nil {
+		output.Err = err.Error()
+		return
+	}
+	if err := client.Add(option.K, n); err != nil {
+		output.Err = err.Error()
+		return
+	}
+}
+
+func serveDec(option *ccdb.Option, output *ccdb.Output) {
+	var n int64
+	if err := json.Unmarshal(option.V, &n); err != nil {
+		output.Err = err.Error()
+		return
+	}
+	if err := client.Dec(option.K, n); err != nil {
+		output.Err = err.Error()
+		return
+	}
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
 	option := &ccdb.Option{}
 	if err := json.NewDecoder(r.Body).Decode(option); err != nil {
 		return
@@ -34,60 +80,37 @@ func hand(w http.ResponseWriter, r *http.Request) {
 
 	switch strings.ToUpper(option.Command) {
 	case "GET":
-		var raw json.RawMessage
-		if err := client.Get(option.K, &raw); err != nil {
-			output.Err = err.Error()
-			return
-		}
-		output.V = raw
-		return
+		serveGet(option, output)
 	case "SET":
-		if err := client.Set(option.K, json.RawMessage(option.V)); err != nil {
-			output.Err = err.Error()
-			return
-		}
-		output.V = option.V
-		return
+		serveSet(option, output)
 	case "DEL":
-		client.Del(option.K)
-		return
+		serveDel(option, output)
 	case "ADD":
-		var n int64
-		if err := json.Unmarshal(option.V, &n); err != nil {
-			output.Err = err.Error()
-			return
-		}
-		if err := client.Add(option.K, n); err != nil {
-			output.Err = err.Error()
-			return
-		}
+		serveAdd(option, output)
 	case "DEC":
-		var n int64
-		if err := json.Unmarshal(option.V, &n); err != nil {
-			output.Err = err.Error()
-			return
-		}
-		if err := client.Dec(option.K, n); err != nil {
-			output.Err = err.Error()
-			return
-		}
+		serveDec(option, output)
 	}
 }
 
 func main() {
 	flag.Parse()
-	switch *flDriver {
-	case "mem":
-		client = acdb.Mem()
-	case "doc":
-		client = acdb.Doc(*flDoc)
-	case "lru":
-		client = acdb.LRU(*flCap)
-	case "map":
-		client = acdb.Map(*flDoc)
-	}
+
+	client = func() acdb.Client {
+		if *flDriverMem {
+			return acdb.Mem()
+		} else if *flDriverDoc {
+			return acdb.Doc(*flPath)
+		} else if *flDriverLRU {
+			return acdb.LRU(*flSize)
+		} else if *flDriverMap {
+			return acdb.Map(*flPath)
+		} else {
+			return acdb.LRU(*flSize)
+		}
+	}()
+
 	log.Println("Listen and serve on", *flListen)
-	if err := http.ListenAndServe(*flListen, http.HandlerFunc(hand)); err != nil {
+	if err := http.ListenAndServe(*flListen, http.HandlerFunc(serve)); err != nil {
 		log.Fatalln(err)
 	}
 }
