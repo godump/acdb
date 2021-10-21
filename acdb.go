779a05ed21892cae92ab517351268d08f996cde7
mohanson <mohanson@outlook.com>
2021-10-21 15:56:10 +0800

    Add has/nil api

--- acdb.go
+++ acdb.go
@@ -35,6 +35,7 @@ func NewMemDriver() *MemDriver {
 	}
 }
 
+// Get the value of a key.
 func (d *MemDriver) Get(k string) ([]byte, error) {
 	v, b := d.data[k]
 	if b {
@@ -43,11 +44,13 @@ func (d *MemDriver) Get(k string) ([]byte, error) {
 	return nil, os.ErrNotExist
 }
 
+// Set the value of a key.
 func (d *MemDriver) Set(k string, v []byte) error {
 	d.data[k] = v
 	return nil
 }
 
+// Del the value of a key.
 func (d *MemDriver) Del(k string) error {
 	delete(d.data, k)
 	return nil
@@ -67,23 +70,27 @@ func NewDocDriver(root string) *DocDriver {
 	}
 }
 
+// Get the value of a key.
 func (d *DocDriver) Get(k string) ([]byte, error) {
 	return os.ReadFile(path.Join(d.root, k))
 }
 
+// Set the value of a key.
 func (d *DocDriver) Set(k string, v []byte) error {
 	return os.WriteFile(path.Join(d.root, k), v, 0644)
 }
 
+// Del the value of a key.
 func (d *DocDriver) Del(k string) error {
 	return os.Remove(path.Join(d.root, k))
 }
 
-// In computing, cache algorithms (also frequently called cache replacement algorithms or cache replacement policies)
-// are optimizing instructions, or algorithms, that a computer program or a hardware-maintained structure can utilize
-// in order to manage a cache of information stored on the computer. Caching improves performance by keeping recent or
-// often-used data items in a memory locations that are faster or computationally cheaper to access than normal memory
-// stores. When the cache is full, the algorithm must choose which items to discard to make room for the new ones.
+// LruDriver implemention. In computing, cache algorithms (also frequently called cache replacement algorithms or cache
+// replacement policies) are optimizing instructions, or algorithms, that a computer program or a hardware-maintained
+// structure can utilize in order to manage a cache of information stored on the computer. Caching improves performance
+// by keeping recent or often-used data items in a memory locations that are faster or computationally cheaper to access
+// than normal memory stores. When the cache is full, the algorithm must choose which items to discard to make room for
+// the new ones.
 //
 // Least recently used (LRU), discards the least recently used items first. It has a fixed size(for limit memory usages)
 // and O(1) time lookup.
@@ -98,6 +105,7 @@ func NewLruDriver(size int) *LruDriver {
 	}
 }
 
+// Get the value of a key.
 func (d *LruDriver) Get(k string) ([]byte, error) {
 	v, b := d.data.Get(k)
 	if b {
@@ -106,11 +114,13 @@ func (d *LruDriver) Get(k string) ([]byte, error) {
 	return nil, os.ErrNotExist
 }
 
+// Set the value of a key.
 func (d *LruDriver) Set(k string, v []byte) error {
 	d.data.Set(k, v)
 	return nil
 }
 
+// Del the value of a key.
 func (d *LruDriver) Del(k string) error {
 	d.data.Del(k)
 	return nil
@@ -131,6 +141,7 @@ func NewMapDriver(root string) *MapDriver {
 	}
 }
 
+// Get the value of a key.
 func (d *MapDriver) Get(k string) ([]byte, error) {
 	var (
 		buf []byte
@@ -148,6 +159,7 @@ func (d *MapDriver) Get(k string) ([]byte, error) {
 	return buf, err
 }
 
+// Set the value of a key.
 func (d *MapDriver) Set(k string, v []byte) error {
 	if err := d.lru.Set(k, v); err != nil {
 		return err
@@ -158,6 +170,7 @@ func (d *MapDriver) Set(k string, v []byte) error {
 	return nil
 }
 
+// Del the value of a key.
 func (d *MapDriver) Del(k string) error {
 	if err := d.lru.Del(k); err != nil {
 		return err
@@ -179,12 +192,14 @@ func NewClient(driver Driver) *Client {
 	return &Client{driver: driver, m: &sync.Mutex{}}
 }
 
+// Get the value of a key.
 func (e *Client) Get(k string) ([]byte, error) {
 	e.m.Lock()
 	defer e.m.Unlock()
 	return e.driver.Get(k)
 }
 
+// Set the value of a key.
 func (e *Client) Set(k string, v []byte) error {
 	e.m.Lock()
 	defer e.m.Unlock()
@@ -192,6 +207,7 @@ func (e *Client) Set(k string, v []byte) error {
 	return e.driver.Set(k, v)
 }
 
+// GetDecode get the decoded value of a key.
 func (e *Client) GetDecode(k string, v interface{}) error {
 	b, err := e.Get(k)
 	if err != nil {
@@ -200,6 +216,7 @@ func (e *Client) GetDecode(k string, v interface{}) error {
 	return json.Unmarshal(b, v)
 }
 
+// SetEncode set the encoded value of a key.
 func (e *Client) SetEncode(k string, v interface{}) error {
 	b, err := json.Marshal(v)
 	if err != nil {
@@ -208,40 +225,58 @@ func (e *Client) SetEncode(k string, v interface{}) error {
 	return e.Set(k, b)
 }
 
+// GetUint32 get the uint32 value of a key.
 func (e *Client) GetUint32(k string) (uint32, error) {
 	var r uint32
 	err := e.GetDecode(k, &r)
 	return r, err
 }
 
+// GetUint64 get the uint64 value of a key.
 func (e *Client) GetUint64(k string) (uint64, error) {
 	var r uint64
 	err := e.GetDecode(k, &r)
 	return r, err
 }
 
+// GetFloat32 get the float32 value of a key.
 func (e *Client) GetFloat32(k string) (float32, error) {
 	var r float32
 	err := e.GetDecode(k, &r)
 	return r, err
 }
 
+// GetFloat64 get the float64 value of a key.
 func (e *Client) GetFloat64(k string) (float64, error) {
 	var r float64
 	err := e.GetDecode(k, &r)
 	return r, err
 }
 
+// GetString get the string value of a key.
 func (e *Client) GetString(k string) (string, error) {
 	var r string
 	err := e.GetDecode(k, &r)
 	return r, err
 }
 
+// Del the value of a key.
 func (e *Client) Del(k string) error {
 	e.m.Lock()
 	defer e.m.Unlock()
 	return e.driver.Del(k)
+}
+
+// Has determine if a key exists.
+func (e *Client) Has(k string) bool {
+	_, err := e.Get(k)
+	return err == nil
+}
+
+// Nil determine if a key emptys.
+func (e *Client) Nil(k string) bool {
+	_, err := e.Get(k)
+	return err != nil
 }
 
 // Mem returns a concurrency-safety Client with MemDriver.
